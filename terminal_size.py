e9129eb12c94342b624a3e3f210f03cd7396d3ec
Koustava Medya <koustavamedya.2000@gmail.com>
2025-10-13 02:58:06 +0530

    Update terminal_size.py

--- terminal_size.py
+++ terminal_size.py
@@ -1,5 +1,5 @@
 import os, shutil
-try:
-    print("os.get_terminal_size():", os.get_terminal_size())
-except OSError as e:
+print("os.get_terminal_size():", os.get_terminal_size())
+"""except OSError as e:
     print("shutil.get_terminal_size():", shutil.get_terminal_size())
+"""
